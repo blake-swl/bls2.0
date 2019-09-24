f119c415e1053f1f3a7c05c0076e31aee8337bf0
blake-swl <seulwoolee93@gmail.com>
2019-09-24 01:48:51 -0700

    [add] setting dev environment

---
+++ public/src/components/App/index.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import ScrollToTop from '../Utils/scrollToTop';
+
+import withSplashScreen from '../Loader/withSplashScreen';
+
+
+import Loader from '../Loader';
+import Navbar from '../Navbar';
+import Menu from '../OverlayMenu'
+import Works from '../Works';
+import Services from '../Services';
+import Contact from '../Contact';
+import Home from '../Home';
+import Footer from '../Footer';
+
+const App = () => {
+
+  return (
+    <div>
+      <ScrollToTop>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" render={props => <Home {...props} />}/>
+          <Route exact path="/services" render={props => <Services {...props} />}/>
+          <Route exact path="/works" render={props => <Works {...props} />}/>
+        </Switch>
+        <Footer />
+      </ScrollToTop>
+    </div>
+  )
+};
+export default App;
+
+// export default withSplashScreen(App);
