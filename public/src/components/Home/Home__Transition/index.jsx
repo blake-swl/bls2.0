30ba2b4be716e369cd7655fa27774635aa2c7370
blake-swl <seulwoolee93@gmail.com>
2019-09-29 16:03:17 -0700

    [edit] landing transition animation

--- public/src/components/Home/Home__Transition/index.jsx
+++ public/src/components/Home/Home__Transition/index.jsx
@@ -6,6 +6,8 @@ import halloween from '../../../../dist/assets/horror.png';
 import dodgers from '../../../../dist/assets/Landing.png';
 import blog from '../../../../dist/assets/blog.png';
 import ascend from '../../../../dist/assets/Ascend.png';
+import menu from '../../../../dist/assets/Artboard1.png';
+
 
 
 
@@ -14,11 +16,16 @@ const Transition = () => (
   <div className="home__transition">
     <p className="home__transition__text">create. inspire. ascend</p>
     <div className="home__transition__images">
-      <div className="transition__img" style={{backgroundImage: `url(${halloween})`}}></div>
-      <div className="transition__img" style={{backgroundImage: `url(${dodgers})`}}></div>
-      <div className="transition__img" style={{backgroundImage: `url(${blog})`}}></div>
-      <div className="transition__img" style={{backgroundImage: `url(${ascend})`}}></div>
-
+      <div className="home__transition__images--inner">
+        <div className="transition__img" style={{backgroundImage: `url(${ascend})`}}></div>
+        <div className="transition__img" style={{backgroundImage: `url(${halloween})`}}></div>
+        <div className="transition__img" style={{backgroundImage: `url(${ascend})`}}></div>
+        <div className="transition__img" style={{backgroundImage: `url(${blog})`}}></div>
+        <div className="transition__img" style={{backgroundImage: `url(${dodgers})`}}></div>
+        <div className="transition__img" style={{backgroundImage: `url(${halloween})`}}></div>
+        <div className="transition__img" style={{backgroundImage: `url(${menu})`}}></div>
+        <div className="transition__img" style={{backgroundImage: `url(${blog})`}}></div>
+      </div>
     </div>
   </div>
 );
